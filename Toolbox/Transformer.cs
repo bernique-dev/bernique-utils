ba3854bc9a83cfa8a3a95e6094f05d2f8adf344c
Bernique <bernique.dev@gmail.com>
2023-07-14 22:02:57 +0200

    MinMax folder + fix Editor exclusion from build compilation

--- Toolbox/Transformer.cs
+++ Toolbox/Transformer.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Transformer {
-    
-    public static Vector2 GetVector2(this Vector3 vector) {
-        return new Vector2(vector.x, vector.y);
-    }
-    public static Vector3 GetVector3(this Vector2 vector) {
-        return new Vector3(vector.x, vector.y, 0);
+namespace BerniqueUtils {
+
+    public static class Transformer {
+
+        public static Vector2 GetVector2(this Vector3 vector) {
+            return new Vector2(vector.x, vector.y);
+        }
+        public static Vector3 GetVector3(this Vector2 vector) {
+            return new Vector3(vector.x, vector.y, 0);
+        }
+
     }
 
 }
